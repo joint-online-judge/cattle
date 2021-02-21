0666eb320a9a4b2677991a70cc0d3ccffe7d37dc
makersmelx <makersmelx@gmail.com>
2021-02-21 16:03:40 +0800

    set react-router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader/root';
 import { Router, Route, Switch } from 'react-router';
-import { TodoContainer } from 'app/containers/TodoContainer';
+import { LoginContainer, TodoContainer } from 'app/containers';
 
 // render react DOM
 export const App = hot(({ history }) => {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" component={TodoContainer} />
+        <Route exact path="/" component={TodoContainer} />
+        <Route path="/login" component={LoginContainer} />
       </Switch>
+
     </Router>
   );
 });
