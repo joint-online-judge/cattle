d73b80eb0f74602203e3993a8454bcd2ac5c7bf2
nichujie <ncj19991213@126.com>
2021-05-12 14:09:20 +0800

    feat(*): user menu

--- src/components/Header/UserMenuItem.tsx
+++ src/components/Header/UserMenuItem.tsx
@@ -1,78 +1,57 @@
 import React, { ReactNode } from 'react';
 import { useModel, useIntl } from 'umi';
-import { Dropdown, Menu } from 'antd';
+import { Dropdown, Menu, Avatar, Space } from 'antd';
 import { Link } from 'react-router-dom';
 import { gravatarImageUrl } from '@/utils';
 import { DownOutlined } from '@ant-design/icons';
 import style from './style.css';
 
-interface LoggedInSubMenuItem {
-  key: string;
-  path: string;
-  node?: ReactNode,
-}
-
 export const Index: React.FC = () => {
   const { initialState } = useModel('@@initialState');
   const intl = useIntl();
 
-  // divider name has no actual meaning just to be a key of the divider component to pass eslint
-  const LoggedInSubMenuArrange: (LoggedInSubMenuItem | string)[] = [
-    {
-      key: 'username',
-      path: `/user/${initialState?.user?.uname || ''}`,
-      node: (<b>{initialState?.user?.uname || ''}</b>),
-    },
-    'username-profile-divider',
-    {
-      key: 'USER.PROFILE',
-      path: `/user/${initialState?.user?.uname || ''}`,
-    },
-    'profile-setting-divider',
-    {
-      key: 'SETTINGS.SETTINGS',
-      path: '/settings',
-    },
-    {
-      key: 'USER.LOG_OUT',
-      path: '/logout',
-    },
-  ];
-  const UserSubMenu = (
-    <Menu className={style.headerUserSubMenu}>
-      {
-        LoggedInSubMenuArrange.map((item) => (typeof (item) !== 'string'
-          ? (
-            <Menu.Item key={item.key} className={style.headerUserSubMenuItem}>
-              <Link to={item.path}>
-                {
-                  item.node ? item.node : intl.formatMessage({ id: item.key })
-                }
-              </Link>
-            </Menu.Item>
-          ) : (
-            <Menu.Divider key={item} />
-          )))
-      }
+  const subMenu = (
+    <Menu>
+      <Menu.Item key="username">
+        <Link to={`/user/${initialState?.user?.uname || ''}`}>
+          <b>{initialState?.user?.uname || ''}</b>
+        </Link>
+      </Menu.Item>
+      <Menu.Divider key="divider-1" />
+      <Menu.Item key="USER.PROFILE">
+        <Link to={`/user/${initialState?.user?.uname || ''}`}>
+          {intl.formatMessage({ id: 'USER.PROFILE' })}
+        </Link>
+      </Menu.Item>
+      <Menu.Divider key="divider-2" />
+      <Menu.Item key="SETTINGS.SETTINGS">
+        <Link to={'/settings'}>
+          {intl.formatMessage({ id: 'SETTINGS.SETTINGS' })}
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="USER.LOG_OUT">
+        <Link to={'/logout'}>
+          {intl.formatMessage({ id: 'USER.LOG_OUT' })}
+        </Link>
+      </Menu.Item>
     </Menu>
   );
 
   return initialState?.user
     ? (
       <Dropdown
-        overlay={UserSubMenu}
+        overlay={subMenu}
         placement="bottomRight"
         trigger={['click']}
         arrow
       >
-        <span>
-          <img
+        <Space>
+          <Avatar
             src={gravatarImageUrl(initialState?.user?.gravatar || '', 20)}
-            alt={`@${initialState?.user?.uname || ''}`}
-            className={style.gravatar}
+            alt={initialState?.user?.uname || ''}
           />
           <DownOutlined />
-        </span>
+        </Space>
       </Dropdown>
     ) : (
       <Link to="/login">{intl.formatMessage({ id: 'USER.LOGIN.JACCOUNT_LOG_IN' })}</Link>
