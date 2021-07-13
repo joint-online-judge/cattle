98d88623bbfdf4197fef1a0524a3ebfaa02d3d66
nichujie <ncj19991213@126.com>
2021-07-13 17:07:06 +0800

    feat(axios): response interceptor

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -1,4 +1,20 @@
 import { Api } from '@/client';
+import { AxiosResponse, AxiosError } from 'axios';
+import { notification } from 'antd';
 
 export const Horse = new Api();
+
+Horse.instance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    notification.error({
+      message: 'Oops...',
+      description: 'There seems to be something wrong with the server. Please contact the maintainers.',
+    });
+    return Promise.reject(error);
+  },
+);
+
 export * from '@/client';
