d3b73678d430c45b67b44e5cd121f2e5dec477f6
Cheney_Ni <ncj19991213@126.com>
2021-12-06 01:03:07 +0100

    feat: update domain role

--- src/pages/DomainSettings/Member/AddUserModal.tsx
+++ src/pages/DomainSettings/Member/AddUserModal.tsx
@@ -1,6 +1,6 @@
 import React, { MutableRefObject, useEffect } from 'react';
 import { useIntl } from 'umi';
-import { message, Form } from 'antd';
+import { message, Form, Row, Col, Card } from 'antd';
 import {
   ModalForm,
   ModalFormProps,
@@ -9,13 +9,14 @@ import {
 import {
   Horse,
   DomainUserAdd,
+  DomainUserUpdate,
   UserWithDomainRole,
   ErrorCode,
 } from '@/utils/service';
 import { useRequest } from 'ahooks';
 import DomainRoleSelect from '@/components/DomainRoleSelect';
 import UserSearchInput from '@/components/DomainCandidateSearchInput';
-import Highlighter from 'react-highlight-words';
+import Gravatar from '@/components/Gravatar';
 
 interface IProps extends ModalFormProps {
   domainUrl: string;
@@ -61,6 +62,34 @@ const Index: React.FC<IProps> = ({
     },
   );
 
+  const { run: updateUser } = useRequest(
+    async (userId: string, values: DomainUserUpdate) => {
+      const response =
+        await Horse.domain.updateDomainUserApiV1DomainsDomainUsersUserPatch(
+          domainUrl,
+          userId,
+          values,
+        );
+      return response.data;
+    },
+    {
+      manual: true,
+      onSuccess: (res) => {
+        if (res.errorCode === ErrorCode.UserAlreadyInDomainBadRequestError) {
+          message.error('user already in domain');
+        } else if (ErrorCode.UserNotFoundError) {
+          message.error('user not found');
+        } else if (res.errorCode === ErrorCode.Success) {
+          message.success('update user success');
+        }
+        onSuccess();
+      },
+      onError: () => {
+        message.error('add domain user failed');
+      },
+    },
+  );
+
   useEffect(() => {
     if (editingUser) {
       formRef?.current?.setFieldsValue({
@@ -72,11 +101,12 @@ const Index: React.FC<IProps> = ({
 
   return (
     <ModalForm<DomainUserAdd>
-      title="添加用户到域"
+      title={editingUser ? '修改用户角色' : '添加用户到域'}
       width={520}
       isKeyPressSubmit
       onFinish={async (values) => {
-        await addUser(values);
+        if (editingUser) await updateUser(editingUser.id, values);
+        else await addUser(values);
         return true;
       }}
       visible={visible}
@@ -92,7 +122,34 @@ const Index: React.FC<IProps> = ({
           },
         ]}
       >
-        <UserSearchInput domainUrl={domainUrl} autoFocus />
+        {editingUser ? (
+          <Card bodyStyle={{ padding: 12 }}>
+            <Row wrap={false} gutter={12} align="middle">
+              <Col flex="none">
+                <Gravatar gravatar={editingUser.gravatar} />
+              </Col>
+              <Col flex="auto">
+                <Row>
+                  <Col span={24}>
+                    <span>
+                      {editingUser.realName
+                        ? `${editingUser.username} (${editingUser.realName})`
+                        : editingUser.username}
+                    </span>
+                  </Col>
+                  <Col span={24}>
+                    <span className="text-xs">
+                      {(editingUser.studentId ?? '') +
+                        (editingUser.email ? ` - ${editingUser.email}` : '')}
+                    </span>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Card>
+        ) : (
+          <UserSearchInput domainUrl={domainUrl} autoFocus />
+        )}
       </Form.Item>
       <Form.Item
         name="role"
