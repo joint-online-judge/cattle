2eca92320fa6f5e5fbe8efd9d4c5a0662aacd79d
makersmelx <makersmelx@gmail.com>
2021-12-27 17:00:12 -0800

    feat: delete uploading custom avatar and add gravatar preview

--- src/pages/UserSettings/Account/AvatarUpload.tsx
+++ src/pages/UserSettings/Account/AvatarUpload.tsx
@@ -1,107 +1,74 @@
-import {
-  Button,
-  Col,
-  Dropdown,
-  Form,
-  Input,
-  Menu,
-  Modal,
-  Row,
-  Upload,
-} from 'antd';
-import ImgCrop from 'antd-img-crop';
-import React, { useCallback, useState } from 'react';
-import { EditOutlined, MailOutlined, UploadOutlined } from '@ant-design/icons';
+import { Button, Col, Form, Input, Modal, Row } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { MailOutlined } from '@ant-design/icons';
 import Gravatar from '@/components/Gravatar';
-import { UploadFile } from 'antd/lib/upload/interface';
 import { VERTICAL_GUTTER } from '@/constants';
+import { useModel } from '@@/plugin-model/useModel';
 
-export interface ImageUploadProps {
-  defaultSrc?: string // src of the default image displayed
-  onUpload?: (imageUrl: string) => void; // called when an image is uploaded, imageUrl is the url of the image uploaded
-}
-
-const AvatarUpload: React.FC<ImageUploadProps> = ({ defaultSrc, onUpload }) => {
-  const [imageUrl, setImageUrl] = useState(defaultSrc ?? '');
+const AvatarUpload: React.FC = () => {
+  const { initialState } = useModel('@@initialState');
+  const [gravatar, setGravatar] = useState(initialState?.user?.gravatar ?? '');
+  const [preview, setPreview] = useState(initialState?.user?.gravatar ?? '');
   const [modalVisible, setModalVisible] = useState(false);
-  const [form] = Form.useForm();
-  const onChange = useCallback(async (param: { file: UploadFile }) => {
-    let src = param.file.url;
-    if (!src) {
-      src = await new Promise(resolve => {
-        const reader = new FileReader();
-        reader.readAsDataURL(param.file.originFileObj as Blob);
-        reader.onload = () => resolve(reader.result as string);
-      });
-    }
-    if (src) {
-      setImageUrl(src);
-      if (onUpload) {
-        onUpload(src);
-      }
-    }
-  }, [onUpload]);
-  const menu = (
-    <Menu>
-      <Menu.Item key="0" icon={<UploadOutlined />}>
-        <ImgCrop>
-          <Upload
-            onChange={onChange}
-            showUploadList={false}
-          >
-            Upload from local
-          </Upload>
-        </ImgCrop>
-      </Menu.Item>
-      <Menu.Item key="1" icon={<MailOutlined />} onClick={() => {
-        setModalVisible(true);
-      }}>
-        Use Gravatar
-      </Menu.Item>
-    </Menu>
-  );
+
+  useEffect(() => {
+    setGravatar(initialState?.user?.gravatar ?? '');
+    setPreview(initialState?.user?.gravatar ?? '');
+  }, [initialState?.user?.gravatar]);
+
   return (
     <>
       <Row justify="center" gutter={VERTICAL_GUTTER}>
         <Col span={24}>
           <Row justify="center">
-            <Gravatar src={imageUrl} size={150} />
+            <Gravatar src={gravatar} size={150} />
           </Row>
         </Col>
-        <Col span={24}>
-          <Row justify="center">
-            <Dropdown overlay={menu} trigger={['click']}>
-              <Button icon={<EditOutlined />}>Edit</Button>
-            </Dropdown>
-          </Row>
-        </Col>
+        <Button icon={<MailOutlined />} onClick={() => {
+          setModalVisible(true);
+        }}>
+          Update Gravatar
+        </Button>
       </Row>
+
       <Modal
-        title="Use Gravatar"
+        title="Update Gravatar"
         visible={modalVisible}
         onOk={() => {
-          form.submit();
+          // todo: update gravatar use imageUrl
+          setModalVisible(false);
         }}
         onCancel={() => {
           setModalVisible(false);
         }}
       >
-        <Form
-          layout="vertical"
-          style={{ width: '50%' }}
-          form={form}
-          onFinish={(value) => {
-            // todo: update gravatar
-            console.log(value);
-            setModalVisible(false);
-          }}>
-          <Form.Item
-            name="gravatar"
-            label="Gravatar Email Address"
-            rules={[{ required: true }, { type: 'email' }]}>
-            <Input />
-          </Form.Item>
-        </Form>
+        <Row gutter={VERTICAL_GUTTER}>
+          <Col span={24}>
+            <Form
+              layout="inline"
+              onFinish={(value) => {
+                setPreview(value.gravatar);
+              }}>
+              <Form.Item
+                initialValue={preview}
+                name="gravatar"
+                label="Gravatar Email"
+                rules={[{ required: true }, { type: 'email' }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item>
+                <Button htmlType="submit" type="primary">
+                  Preview
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+          <Col span={24}>
+            <Row justify="center">
+              <Gravatar gravatar={preview} size={100} />
+            </Row>
+          </Col>
+        </Row>
       </Modal>
     </>
   );
