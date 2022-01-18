72d1dd7be5c5f0ed8358b3362c57bf31dae7ba96
makersmelx <makersmelx@gmail.com>
2022-01-17 22:31:14 -0500

    misc: delete todo

--- src/components/Gravatar/index.tsx
+++ src/components/Gravatar/index.tsx
@@ -7,7 +7,7 @@ export interface GravatarProps extends AvatarProps {
   src?: string;
   gravatar?: string; // if gravatar is set, use gravatar regardless of src
 }
-// todo: add a loading fallback component
+
 const Index: React.FC<GravatarProps> = (props) => {
   const { gravatar, src, ...otherProps } = props;
   const imageUrl = gravatar ? gravatarImageUrl(gravatar, 300) : src;
