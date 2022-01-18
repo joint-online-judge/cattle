8df8e8022db1871446fb40463eadd1efd7d10b01
makersmelx <makersmelx@gmail.com>
2022-01-18 14:20:35 -0500

    feat: remove the manual preview. The preview gravatar will be loaded once the user stops inputting for 0.5 seconds

--- src/pages/UserSettings/Account/AvatarUpload.tsx
+++ src/pages/UserSettings/Account/AvatarUpload.tsx
@@ -1,5 +1,6 @@
 import { Button, Col, Form, Input, message, Modal, Row } from 'antd';
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useMemo, useState } from 'react';
+import { debounce } from 'lodash';
 import { EditOutlined } from '@ant-design/icons';
 import Gravatar from '@/components/Gravatar';
 import { VERTICAL_GUTTER } from '@/constants';
@@ -8,32 +9,61 @@ import { useRequest } from 'ahooks';
 import Horse from '@/utils/service';
 
 const AvatarUpload: React.FC = () => {
-  const { initialState } = useModel('@@initialState');
+  const { initialState, refresh } = useModel('@@initialState');
   const [preview, setPreview] = useState(initialState?.user?.gravatar ?? '');
   const [modalVisible, setModalVisible] = useState(false);
+  const [okDisabled, setOkDisabled] = useState(false);
   const [form] = Form.useForm();
-
-  useEffect(() => {
-    setPreview(initialState?.user?.gravatar ?? '');
-  }, []);
 
   const { run: changeGravatar, loading } = useRequest(async (url) => {
     await Horse.user.v1UpdateCurrentUser({ gravatar: url });
-    await Horse.auth.v1Refresh({
-      responseType: 'redirect',
-      redirectUrl: window.location.href,
-    });
   }, {
     manual: true,
     onSuccess: async () => {
       message.success('change gravatar success');
-      setModalVisible(false);
-      window.location.reload();
+      await Horse.auth.v1Refresh({
+        responseType: 'json',
+      });
+      await refresh();
     },
     onError: () => {
       message.error('change gravatar fails');
     },
   });
+
+  const debouncedSetPreview = debounce((value: string) => {
+    setPreview(value);
+  }, 500);
+
+  const previewGravatar = useMemo(() => (
+    <Gravatar gravatar={preview} size={100} />
+  ), [preview]);
+
+  const gravatarOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    form.validateFields().then(() => {
+      debouncedSetPreview(e.target.value);
+      setOkDisabled(false);
+    }, () => {
+      if (preview) {
+        setPreview('');
+      }
+      if (!okDisabled) {
+        setOkDisabled(true);
+      }
+    });
+  };
+
+  const initModal = () => {
+    setModalVisible(true);
+    setPreview(initialState?.user?.gravatar ?? '');
+    form.resetFields();
+  };
+
+  const submitPatch = async () => {
+    const values = await form.validateFields();
+    await changeGravatar(values.gravatar);
+    setModalVisible(false);
+  };
 
   return (
     <>
@@ -45,9 +75,8 @@ const AvatarUpload: React.FC = () => {
         </Col>
         <Button
           icon={<EditOutlined />}
-          onClick={() => {
-            setModalVisible(true);
-          }}>
+          onClick={initModal}
+        >
           Change Gravatar
         </Button>
       </Row>
@@ -55,48 +84,33 @@ const AvatarUpload: React.FC = () => {
       <Modal
         title="Change Gravatar"
         visible={modalVisible}
-        okButtonProps={{ loading }}
+        okButtonProps={{ loading, disabled: okDisabled }}
         cancelButtonProps={{ loading }}
-        onOk={async () => {
-          const values = await form.validateFields();
-          await changeGravatar(values.gravatar);
-        }}
+        onOk={submitPatch}
         onCancel={() => {
           setModalVisible(false);
         }}
       >
-        <Row gutter={VERTICAL_GUTTER}>
-          <Col span={24}>
-            <Form
-              layout="inline"
-              form={form}
-              onFinish={(value) => {
-                setPreview(value.gravatar);
-              }}>
-              <Form.Item
-                initialValue={preview}
-                name="gravatar"
-                label="Gravatar Email"
-                rules={[
-                  { required: true },
-                  { type: 'email' },
-                ]}>
-                <Input style={{ minWidth: '200px' }} />
-              </Form.Item>
-              <Form.Item>
-                <Button htmlType="submit">
-                  Preview
-                </Button>
-              </Form.Item>
-            </Form>
-          </Col>
+        <Form
+          layout="vertical"
+          form={form}
+          initialValues={{ gravatar: initialState?.user?.gravatar }}
+        >
+          <Form.Item
+            name="gravatar"
+            label="Gravatar Email"
+            rules={[
+              { required: true },
+              { type: 'email' },
+            ]}
+          >
+            <Input onChange={gravatarOnChange} />
+          </Form.Item>
 
-          <Col span={24}>
-            <Row justify="center">
-              <Gravatar gravatar={preview} size={100} />
-            </Row>
-          </Col>
-        </Row>
+          <Row justify="center">
+            {previewGravatar}
+          </Row>
+        </Form>
       </Modal>
     </>
   );
