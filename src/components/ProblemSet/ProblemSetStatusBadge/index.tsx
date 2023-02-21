7bd27b882c092e0d0a1818619c81fbf9574a167b
nichujie <chujie@kth.se>
2023-02-21 17:40:35 +0100

    feat: show problem set after expired

--- src/components/ProblemSet/ProblemSetStatusBadge/index.tsx
+++ src/components/ProblemSet/ProblemSetStatusBadge/index.tsx
@@ -1,32 +1,44 @@
-import { Badge } from 'antd'
+import { Badge, Tooltip } from 'antd'
 import type React from 'react'
 import { useTranslation } from 'react-i18next'
 import { ProblemSetStatus } from 'types'
 import { getProblemSetStatus } from '../utils'
 
 interface IProps {
-	unlockAt?: string
-	dueAt?: string
-	lockAt?: string
+  unlockAt?: string
+  dueAt?: string
+  lockAt?: string
 }
 
 const Index: React.FC<IProps> = ({ unlockAt, dueAt, lockAt }) => {
-	const { t } = useTranslation()
-	const status = getProblemSetStatus(unlockAt, dueAt, lockAt)
+  const { t } = useTranslation()
+  const status = getProblemSetStatus(unlockAt, dueAt, lockAt)
 
-	if (status === ProblemSetStatus.Unstarted) {
-		return <Badge status='default' text={t('problemSet.status.notStarted')} />
-	}
+  if (status === ProblemSetStatus.Unstarted) {
+    return (
+      <Tooltip title={t('problemSet.tooltip.notStarted')}>
+        <Badge status='default' text={t('problemSet.status.notStarted')} />
+      </Tooltip>
+    )
+  }
 
-	if (status === ProblemSetStatus.Locked) {
-		return <Badge status='error' text={t('problemSet.status.ended')} />
-	}
+  if (status === ProblemSetStatus.Locked) {
+    return (
+      <Tooltip title={t('problemSet.tooltip.ended')}>
+        <Badge status='error' text={t('problemSet.status.ended')} />
+      </Tooltip>
+    )
+  }
 
-	if (status === ProblemSetStatus.Overdue) {
-		return <Badge status='warning' text={t('problemSet.status.overdue')} />
-	}
+  if (status === ProblemSetStatus.Overdue) {
+    return (
+      <Tooltip title={t('problemSet.tooltip.overdue')}>
+        <Badge status='warning' text={t('problemSet.status.overdue')} />
+      </Tooltip>
+    )
+  }
 
-	return <Badge status='processing' text={t('problemSet.status.ongoing')} />
+  return <Badge status='processing' text={t('problemSet.status.ongoing')} />
 }
 
 export default Index
